c5ab3c00704097a21a323778fe60c1fcb40594dd
leonard0-b <leonardo.bongianni@gmail.com>
2021-05-12 18:31:02 +0200

    bozza footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends("layout.main")
+@extends("layout.app")
 
 @section("content")
 <main>
--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,4 +1,57 @@
 <footer>
-	
+    <div class="container flex">
+        <div class="left flex">
+            <div class="col-25">
+                <ul>
+                    <li class="uppercase">dc comics</li>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li clas="uppercase">tv</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+                <ul>
+                    <li class="uppercase">shop</li>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <div class="col-25">
+                <ul>
+                    <li class="uppercase">dc</li>
+                    <li>Term Of Use</li>
+                    <li>Privacy policy (New)</li>
+                    <li>Ad Choices</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshop</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Conact Us</li>
+                </ul>
+            </div>
+            <div class="col-25">    
+                <ul>
+                    <li class="uppercase">sites</li>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+        <div class="right">
+            <img src="/img/dc-logo-bg.png" alt="logo-dc">
+        </div>
+    </div>
+    <section>
+        <div class="container">
 
+        </div>
+    </section>
 </footer>
