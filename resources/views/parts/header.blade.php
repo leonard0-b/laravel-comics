721115dec2e2c9031a74313823fec8f28ce26e75
leonard0-b <leonardo.bongianni@gmail.com>
2021-05-12 16:20:03 +0200

    seconda nav

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -9,7 +9,29 @@
 			</div>
 		</div>
 		<div class="nav-down">
-		
+			<div class="container flex">
+				<div class="logo">
+					<img src="/img/dc-logo.png" alt="dc-logo">
+				</div>
+				<div class="nav-cmd">
+					<ul class="flex">
+						<li><a href="">characters</a></li>
+						<li class="active"><a href="">comics</a></li>
+						<li><a href="">movies</a></li>
+						<li><a href="">tv</a></li>
+						<li><a href="">games</a></li>
+						<li><a href="">collectibles</a></li>
+						<li><a href="">videos</a></li>
+						<li><a href="">fans</a></li>
+						<li><a href="">news</a></li>
+						<li><a href="">shop</a><i class="fas fa-caret-down"></i></li>
+					</ul>
+				</div>
+				<div class="search flex">
+					<input type="text" placeholder="Search">
+					<button><i class="fas fa-search"></i></button>
+				</div>
+			</div>
 		</div>
 	</nav>
 </header>
