e26ab794fdf7531af3e7d6ca6894108e4f99044e
leonard0-b <leonardo.bongianni@gmail.com>
2021-05-12 19:06:40 +0200

    footer

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -34,7 +34,7 @@
                     <li>Conact Us</li>
                 </ul>
             </div>
-            <div class="col-25">    
+            <div class="col-50">    
                 <ul>
                     <li class="uppercase">sites</li>
                     <li>DC</li>
@@ -44,14 +44,27 @@
                     <li>DC Power Visa</li>
                 </ul>
             </div>
+            <div class="cookies">
+                <p>All Sites Content TM and 2020 Dc Entertainment, unless otherwise <a href="">noted here</a>. All rights reserved. <a href="">Cookies Settings</a></p>
+            </div>
         </div>
         <div class="right">
             <img src="/img/dc-logo-bg.png" alt="logo-dc">
         </div>
     </div>
-    <section>
-        <div class="container">
-
+    <section class="followus">
+        <div class="container flex">
+            <div class="follow-left">
+                <a class="uppercase" href="">sign-up now!</a>
+            </div>
+            <div class="follow-right flex">
+                <a href="" class="uppercase">follow us</a>
+                <img src="/img/footer-facebook.png" alt="">
+                <img src="/img/footer-twitter.png" alt="">
+                <img src="/img/footer-youtube.png" alt="">
+                <img src="/img/footer-pinterest.png" alt="">
+                <img src="/img/footer-periscope.png" alt="">
+            </div>
         </div>
     </section>
 </footer>
