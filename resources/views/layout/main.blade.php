5005067828b5cf990b5f170a40db60c3aa802336
leonard0-b <leonardo.bongianni@gmail.com>
2021-05-12 15:20:45 +0200

    prima nav

---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>DcComics</title>
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+        <!-- CSS -->
+        <link rel="stylesheet" href="/css/app.css">
+        <!-- FONTAWESOME -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+    </head>
+    <body>
+        @include("parts.header")
+
+        @include("parts.footer")
+    </body>
+</html>
---
+++ resources/views/parts/header.blade.php
@@ -0,0 +1,15 @@
+<header>
+	<nav>
+		<div class="nav-up">
+			<div class="container flex">
+				<div class="nav-up-text flex">
+					<span class="uppercase">dc power&#8480 visa&#174</span>
+					<span class="uppercase">additional dc sites<i class="fas fa-caret-down"></i></span>
+				</div>
+			</div>
+		</div>
+		<div class="nav-down">
+		
+		</div>
+	</nav>
+</header>
