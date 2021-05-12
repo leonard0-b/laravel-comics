f09970b033c88d7536493710e0d08a86614678aa
leonard0-b <leonardo.bongianni@gmail.com>
2021-05-12 17:53:56 +0200

    main con fumetti da ciclare

---
+++ resources/views/home.blade.php
@@ -0,0 +1,65 @@
+@extends("layout.main")
+
+@section("content")
+<main>
+    <!-- sezione cards -->
+    <section>
+        <div class="container">
+            <div class="wrapper flex">
+                <div class="card">
+                    <div class="img">qui ci va la copertina</div>
+                    <div class="title">qui ci va il titolo</div>
+                </div>
+            </div>
+            <div class="load flex">
+                <a class="uppercase" href="">load more</a>
+            </div>
+        </div>
+    </section>
+    <!-- sezione banner-blue -->
+    <section class="blue">
+        <div class="container flex">
+            <div class="cell flex">
+                <div class="img">
+                    <img src="/img/buy-comics-digital-comics.png" alt="digital-comics">
+                </div>
+                <div class="cell-txt flex">
+                    <p class="uppercase">digital comics</p>
+                </div>
+            </div>
+            <div class="cell flex">
+                <div class="img">
+                    <img src="/img/buy-comics-merchandise.png" alt="digital-comics">
+                </div>
+                <div class="cell-txt flex">
+                    <p class="uppercase">dc merchandise</p>
+                </div>
+            </div>
+            <div class="cell flex">
+                <div class="img">
+                    <img src="/img/buy-comics-subscriptions.png" alt="digital-comics">
+                </div>
+                <div class="cell-txt flex">
+                    <p class="uppercase">subscription</p>
+                </div>
+            </div>
+            <div class="cell flex">
+                <div class="img">
+                    <img src="/img/buy-comics-shop-locator.png" alt="digital-comics">
+                </div>
+                <div class="cell-txt flex">
+                    <p class="uppercase">comic shop locator</p>
+                </div>
+            </div>
+            <div class="cell flex">
+                <div class="img">
+                    <img src="/img/buy-dc-power-visa.svg" alt="digital-comics">
+                </div>
+                <div class="cell-txt flex">
+                    <p class="uppercase">dc power visa</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</main>
+@endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>DcComics</title>
+        <title>DCComics</title>
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
@@ -13,8 +13,15 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
     </head>
     <body>
+        <!-- includo la parte dell' header -->
         @include("parts.header")
 
+        
+        @yield("content")
+        
+
+
+        <!-- includo la parte del footer -->
         @include("parts.footer")
     </body>
 </html>
